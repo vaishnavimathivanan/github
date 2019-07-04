36faf040b60e23a530c37b2157f5555f000e1802
ITSUPPORT <ITSUPPORT@MDTR09.mlad.in>
2019-07-04 17:10:45 +0600

    first commit

---
+++ maven-project1/src/main/java/com/selenium/test1/maven_project1/App.java
@@ -0,0 +1,13 @@
+package com.selenium.test1.maven_project1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ maven-project1/src/main/java/com/selenium/test1/maven_project1/Maven.java
@@ -0,0 +1,29 @@
+package com.selenium.test1.maven_project1;
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Maven {
+	public static void main(String []args) throws InterruptedException, IOException{
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Administrator\\Desktop\\selenium download chromedriver\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://connect.maveric-systems.com/index.php/site/login");
+		WebElement ele=driver.findElement(By.id("LoginForm_username"));
+	    ele.sendKeys("vaishnavima");
+	    WebElement ele1=driver.findElement(By.id("LoginForm_password"));
+		ele1.sendKeys("v@ishu123");
+		WebElement ele2=driver.findElement(By.name("yt0"));
+		ele2.click();
+		Thread.sleep(5000);
+		 
+		 
+		
+    
+	
+	}
+}
+
+
